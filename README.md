6385060ec753a63280f8f0fd416e94a4438f7271
Koen Verbruggen <Verbruggen.Koen@gmail.com>
2019-10-17 00:17:39 +0200

    UFW for Elrond BoN - initial

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Elrond-scripts
 Some scripts for Elrond Battle of Nodes
+
+## Installation
+Works on Debian / Ubuntu.
+`bash ufw.sh`
+
+## Description
+* You need to run this as root or with sudo.
+* It reads your sshd_config for custom port, otherwise 22 default.
+* Default : incoming drop & outgoing accept.
+* Exceptions : sshd + ratelimiting.
---
+++ ufw.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# CHECK root
+if ! [ $(id -u) = 0 ]; then
+   echo "Script must be run as root / sudo."
+   exit 1
+fi
+
+# Read SSH port from config file, otherwise default 22
+_sshd_input=$(cat /etc/ssh/sshd_config | awk '/^Port/ {print $2}')
+re='^[0-9]+$'
+if [[ $_sshd_input =~ $re ]]; then
+  # is number
+  if [ $_sshd_input -le 65535 ] && [ $_sshd_input -gt 0 ]; then
+    _sshPort=$_sshd_input
+  else
+    _sshPort="22"
+  fi
+fi
+
+#Firewall security measures
+apt install ufw -y
+ufw disable
+ufw allow ${_sshPort}/tcp
+ufw limit ${_sshPort}/tcp
+ufw logging on
+ufw default deny incoming
+ufw default allow outgoing
+ufw --force enable
+
+echo "Success! Make sure to test a NEW ssh connection before disconnecting your old session or rebooting."
